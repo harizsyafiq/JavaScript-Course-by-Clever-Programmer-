e774f3241896d066863b9752f74cd39f0597a916
hrzSyfq <59418169+harizsyafiq@users.noreply.github.com>
2023-02-15 22:47:31 +0800

    Update script.js

--- functions/sum/solution/script.js
+++ functions/sum/solution/script.js
@@ -1,1 +1,43 @@
-console.log("Sum solution"))
+/* 
+  TODO: Write a function that sums two numbers
+  TODO: Write a function that subtracts two numbers
+  TODO: Write a function that divides two numbers.
+  TODO: Write a function that multiplies two numbers.
+  *NOTE* be sure to handle dividing by zero 😉
+  ES5 Syntax: function Add(){}
+  ES6 Syntax (Arrow function): const add = () => {}
+*/
+
+function add(a, b) {
+  return a + b
+}
+
+const add1 = (a, b) => a + b
+
+function sub(a, b) {
+  return a - b
+}
+
+const sub1 = (a, b) => a - b
+
+function div(a, b) {
+  return a / b
+}
+
+const div1 = (a,b) => a / b
+
+function mul(a, b) {
+  return a * b
+}
+
+const mul1 = (a,b) => a * b
+
+console.log('hello from the SUM exercise')
+/*
+  TODO: create a function that console logs the result of any of the above operations.
+*/
+
+console.log(add1(3,6))
+console.log(sub1(3,6))
+console.log(div1(6,0))
+console.log(mul1(3,6))
